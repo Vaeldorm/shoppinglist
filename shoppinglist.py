c4da4cbfb758d60a0b1ad2bdc2cdc730bb2aa776
Dylan 07 <Dylan.Skinkis@gmail.com>
2023-01-20 19:27:56 -0600

    Update shoppinglist.py

--- shoppinglist.py
+++ shoppinglist.py
@@ -1,36 +1,61 @@
-# Main function will consist of a list of variables named as ingredients (i.e. chicken), as well as the print functions that the user will need at the end of their interaction to see all the items they will need to buy to make the meals they selected. The main function will also call on other functions to prompt the user and gather this aforementioned information.
-# def main():
+def print_menu(dishes):
+        for idx, dish in enumerate(dishes):
+            print(f"{idx + 1}. {dish['name']}")
 
-# for loop to update ingredients
-def update_count():
-    for dish in dishes:
-        if dish == "Chicken":
-            chicken_count += 1
-        elif dish == "Ground Beef":
-            ground_beef_count += 1
+def get_ingredients_from_dish(selected_dish):
+    name = selected_dish["name"]
+    ingredients = [
+        ingredient
+        for ingredient in selected_dish.values()
+        if ingredient != name
+    ]
+    return ingredients
 
-# get selection from user
+def add_ingredients_to_shopping_list(ingredient, shopping_list):
+    if ingredient in shopping_list:
+        shopping_list[ingredient] += 1
+    else:
+        shopping_list[ingredient] = 1
+    return shopping_list
+
+def main():
+    shopping_list = {}
+    dishes = [
+        {
+            "name": "Cajun Chicken Pasta",
+            "ingredient1": "Chicken",
+            "ingredient2": "Pasta",
+            "ingredient3": "Red Bell Pepper",
+            "ingredient4": "Green Bell Pepper",
+            "ingredient4": "Cherry Tomatoes",
+        },
+        {
+            "name": "Pesto Chicken Pasta",
+            "ingredient1": "Chicken",
+            "ingredient2": "Pasta",
+            "ingredient3": "Basil",
+            "ingredient4": "Pine Nuts",
+            "ingredient4": "Cherry Tomatoes",
+            "ingredient5": "Parmesan Cheese",
+            "ingredient6": "Garlic",
+        },
+    ]
+    print_menu(dishes)
+
+    while True:
+        choice = input("Please type the number for the meal you would like to cook, or 'q' to quit. ")
+        if choice == "q":
+            break
+        try:
+            choice = int(choice)
+            selected_dish = dishes[choice - 1]
+            ingredients = get_ingredients_from_dish(selected_dish)
+            for ingredient in ingredients:
+                shopping_list = add_ingredients_to_shopping_list(ingredient, shopping_list)
+        except (ValueError, IndexError):
+            print("Invalid choice. Please enter a valid number or 'q' to quit. ")
+
+    print(shopping_list)
 
 
-def get_dish():
-    print("Please choose a number:")
-    for dish in dishes:
-        print(dish["number"], dish["name"], sep=(" "))
-    return int(input())
-
-
-# main dict
-# "name":"dish", "ingredient1": "item", "ingredient2": "item", "ingredient3": "item", "ingredient4": "item", "ingredient4": "item"
-dishes = [
-    {"number": 1, "name": "Cajun Chicken Pasta", "ingredient1": "Chicken", "ingredient2": "Pasta",
-        "ingredient3": "Red Bell Pepper", "ingredient4": "Green Bell Pepper", "ingredient4": "Cherry Tomatoes"},
-    {"number": 2, "name": "Pesto Chicken Pasta", "ingredient1": "Chicken", "ingredient2": "Pasta", "ingredient3": "Basil",
-        "ingredient4": "Pine Nuts", "ingredient4": "Cherry Tomatoes", "ingredient5": "Parmesan Cheese", "ingredient6": "Garlic"}
-]
-
-selection = get_dish()
-chicken_count = 0
-ground_beef_count = 0
-
-# Test
-# print(selection)
+main()
